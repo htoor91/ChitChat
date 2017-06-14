5c07be260db2b642b2d717aa0d9d248c54b40949
Hassan Toor <hassantoor8@gmail.com>
2017-06-14 14:27:38 -0400

    Add user crud

---
+++ server/api/user/userController.js
@@ -0,0 +1,78 @@
+const User = require('./userModel');
+const _ = require('lodash');
+const signToken = require('../../auth/auth').signToken;
+
+exports.params = function(req, res, next, id) {
+  User.findById(id)
+    .select('-password')
+    .exec()
+    .then(function(user) {
+      if (!user) {
+        next(new Error('No user with that id'));
+      } else {
+        req.user = user;
+        next();
+      }
+    }, function(err) {
+      next(err);
+    });
+};
+
+exports.get = function(req, res, next) {
+  User.find({})
+    .select('-password')
+    .exec()
+    .then(function(users){
+      res.json(users.map(function(user){
+        return user.toJson();
+      }));
+    }, function(err){
+      next(err);
+    });
+};
+
+exports.getOne = function(req, res, next) {
+  const user = req.user.toJson();
+  res.json(user.toJson());
+};
+
+exports.put = function(req, res, next) {
+  const user = req.user;
+
+  const update = req.body;
+
+  _.merge(user, update);
+
+  user.save(function(err, saved) {
+    if (err) {
+      next(err);
+    } else {
+      res.json(saved.toJson());
+    }
+  });
+};
+
+exports.post = function(req, res, next) {
+  const newUser = new User(req.body);
+
+  newUser.save(function(err, user) {
+    if(err) { return next(err);}
+
+    const token = signToken(user._id);
+    res.json({token: token});
+  });
+};
+
+exports.delete = function(req, res, next) {
+  req.user.remove(function(err, removed) {
+    if (err) {
+      next(err);
+    } else {
+      res.json(removed.toJson());
+    }
+  });
+};
+
+exports.me = function(req, res) {
+  res.json(req.user.toJson());
+};
