ee45e9e56b1da8d1d40cd81111d3b545e55f4330
Hassan Toor <hassantoor8@gmail.com>
2017-06-14 11:54:59 -0400

    Configure server structure

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const api = require('./api/api');
+const config = require('./config/config');
+const logger = require('./util/logger');
+const auth = require('./auth/routes');
+// connect to DB
+require('mongoose').connect(config.db.url);
+
+if (config.seed) {
+  require('./util/seed');
+}
+// setup the app middlware
+require('./middleware/appMiddlware')(app);
+
+// setup the api
+app.use('/api', api);
+app.use('/auth', auth);
+// set up global error handling
+
+app.use(function(err, req, res, next) {
+  // if error thrown from jwt validation check
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send('Invalid token');
+    return;
+  }
+
+  logger.error(err.stack);
+  res.status(500).send('Oops');
+});
+
+// export the app for testing
+module.exports = app;
