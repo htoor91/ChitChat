c085ae99a6c14e981f28d0fce64002d816dcd3dc
Hassan Toor <hassantoor8@gmail.com>
2017-06-14 12:00:42 -0400

    Configure global middleware

---
+++ server/middleware/appMiddleware.js
@@ -0,0 +1,13 @@
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const override = require('method-override');
+// setup global middleware here
+
+module.exports = function(app) {
+  app.use(morgan('dev'));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(cors());
+  app.use(override());
+};
