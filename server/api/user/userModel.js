607ded115f342997bdc60105a48c33e8a9624cc7
Hassan Toor <hassantoor8@gmail.com>
2017-06-14 14:22:54 -0400

    Complete user schema

---
+++ server/api/user/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  // dont store the password as plain text
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+UserSchema.pre('save', function(next) {
+  if (!this.isModified('password')) return next();
+
+
+  this.password = this.encryptPassword(this.password);
+  next();
+});
+
+UserSchema.methods = {
+  // check the passwords on signin
+  authenticate: function(plainTextPword) {
+    return bcrypt.compareSync(plainTextPword, this.password);
+  },
+  // hash the passwords
+  encryptPassword: function(plainTextPword) {
+    if (!plainTextPword) {
+      return '';
+    } else {
+      const salt = bcrypt.genSaltSync(10);
+      return bcrypt.hashSync(plainTextPword, salt);
+    }
+  },
+
+  toJson: function() {
+    const obj = this.toObject();
+    delete obj.password;
+    return obj;
+  }
+};
+
+
+module.exports = mongoose.model('user', UserSchema);
