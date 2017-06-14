e8d5e2866895c5cda48115b0d0b9a0a7e764e13b
Hassan Toor <hassantoor8@gmail.com>
2017-06-14 14:19:14 -0400

    Fix var scopes

--- server/auth/controller.js
+++ server/auth/controller.js
@@ -1,10 +1,10 @@
-var User = require('../api/user/userModel');
-var signToken = require('./auth').signToken;
+const User = require('../api/user/userModel');
+const signToken = require('./auth').signToken;
 
 exports.signin = function(req, res, next) {
   // req.user will be there from the middleware
   // verify user. Then we can just create a token
   // and send it back for the client to consume
-  var token = signToken(req.user._id);
+  const token = signToken(req.user._id);
   res.json({token: token});
 };
