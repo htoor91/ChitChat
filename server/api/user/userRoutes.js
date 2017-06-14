a154b3bf9ce3e956c879bca4d01957a92434d15a
Hassan Toor <hassantoor8@gmail.com>
2017-06-14 14:27:48 -0400

    Add user routes

---
+++ server/api/user/userRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const logger = require('../../util/logger');
+const controller = require('./userController');
+const auth = require('../../auth/auth');
+const checkUser = [auth.decodeToken(), auth.getFreshUser()];
+
+// setup boilerplate route jsut to satisfy a request
+// for building
+router.param('id', controller.params);
+router.get('/me', checkUser, controller.me);
+
+router.route('/')
+  .get(controller.get)
+  .post(controller.post);
+
+router.route('/:id')
+  .get(controller.getOne)
+  .put(checkUser, controller.put)
+  .delete(checkUser, controller.delete);
+
+module.exports = router;
