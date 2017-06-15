468d86385ac1ff37795370a4c9f418aad8fd6188
Hassan Toor <hassantoor8@gmail.com>
2017-06-15 15:58:47 -0400

    Fix import

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'react-thunk';
-import rootReducer from '../reducers/rootReducer';
+import rootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => {
   return createStore(rootReducer, preloadedState, applyMiddleware(thunk));
