0af59828b53f192c5d161069988639388c0279f6
Hassan Toor <hassantoor8@gmail.com>
2017-06-16 20:46:20 -0400

    Create test components

---
+++ client/src/components/home/home_container.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomeContainer = () => {
+  return(
+    <h1>Home Container</h1>
+  );
+};
+
+export default HomeContainer;
---
+++ client/src/components/splash/splash_container.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SplashContainer = () => {
+  return(
+    <h1>Splash Container</h1>
+  );
+};
+
+export default SplashContainer;
