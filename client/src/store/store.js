553a8ec3a28c251f9164663ccf2a665fef700bd9
Hassan Toor <hassantoor8@gmail.com>
2017-06-16 20:44:12 -0400

    Configure store with proper middleware and reducers

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,7 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'react-thunk';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 
+const logger = createLogger();
+
 const configureStore = (preloadedState = {}) => {
-  return createStore(rootReducer, preloadedState, applyMiddleware(thunk));
+  return createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(thunk, logger));
 };
+
+export default configureStore;
