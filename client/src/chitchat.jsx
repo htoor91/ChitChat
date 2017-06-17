65f15dadfac6a9b1ec164530a85f841093e2e09c
Hassan Toor <hassantoor8@gmail.com>
2017-06-16 20:43:38 -0400

    Setup store and render root

--- client/src/chitchat.jsx
+++ client/src/chitchat.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configureStore from './store/store';
 
-const Root = () => {
-  return (
-    <h1>Hello World</h1>
-  );
-};
 
-document.addEventListener('DOMContentLoaded', function(){
-  ReactDOM.render(<Root />, document.getElementById('main'));
+document.addEventListener("DOMContentLoaded", () => {
+  var store;
+  const currentUser = localStorage.getItem('currentUser');
+  const main = document.getElementById("main");
+  
+  if (currentUser) {
+    const preloadedState = { auth: { currentUser: currentUser } };
+    store = configureStore(preloadedState);
+  } else {
+    store = configureStore();
+  }
+  ReactDOM.render(<Root store={ store } />, main);
 });
