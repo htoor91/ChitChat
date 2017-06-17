102b56f9bbc5afcdebc6ffdc432e920caadc6d1f
Hassan Toor <hassantoor8@gmail.com>
2017-06-16 20:47:10 -0400

    Configure auth reducer

---
+++ client/src/reducers/auth_reducer.js
@@ -0,0 +1,32 @@
+import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS, CLEAR_ERRORS } from '../actions/auth_actions';
+import merge from 'lodash/merge';
+
+const initState = {
+  currentUser: null,
+  errors: [],
+};
+
+const AuthReducer = (state = initState, action) => {
+  const nextState = merge({}, state);
+	Object.freeze(state);
+
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      return {
+        currentUser: action.currentUser,
+        errors: []
+      };
+    case RECEIVE_ERRORS:
+      return {
+        currentUser: null,
+        errors: action.errors,
+      };
+    case CLEAR_ERRORS:
+      nextState.errors = [];
+      return nextState;
+    default:
+      return state;
+  }
+};
+
+export default AuthReducer;
