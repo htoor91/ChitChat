80e00cad5d91f16b47b53eb0ed6088de659fefd9
Hassan Toor <hassantoor8@gmail.com>
2017-06-19 19:02:18 -0400

    Create auth form container

---
+++ client/src/components/splash/auth_form/auth_form_container.js
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import { login, signup, clearErrors } from '../../../actions/auth_actions';
+import AuthForm from './auth_form';
+
+const mapStateToProps = (state, { match }) => ({
+  loggedIn: Boolean(state.auth.currentUser),
+  loginErrors: state.auth.errors,
+  path: match.path
+});
+
+const mapDispatchToProps = (dispatch, { location }) => {
+  const formType = location.pathname.slice(1);
+  const processForm = (formType === 'login') ? login : signup;
+
+  return {
+    processForm: (user) => dispatch(processForm(user)),
+    clearErrors: () => dispatch(clearErrors()),
+    signup: (user) => dispatch(signup(user))
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(AuthForm));
