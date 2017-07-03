f9b85700bf9d6c4d91a7bd0212e5b34e57fd4099
Hassan Toor <hassantoor8@gmail.com>
2017-07-03 17:22:39 -0400

    Add channel api util

---
+++ client/src/util/channel_util.js
@@ -0,0 +1,35 @@
+const ChannelUtil = {
+
+  createChannel(channel){
+    return $.ajax({
+      method: "POST",
+      url: "/api/users",
+      data: {channel}
+    });
+  },
+
+  fetchUserChannels(userId){
+    return $.ajax({
+    method: 'GET',
+    url: `/api/users/${userId}/channels`,
+    });
+  },
+
+  deleteChannel(id){
+    return $.ajax({
+    method: 'DELETE',
+    url: `/api/channels/${id}`
+    });
+  },
+
+  fetchChannelUserCount(channelId){
+    return $.ajax({
+    method: 'GET',
+    url: `/api/channels/${channelId}`
+    });
+  }
+
+
+};
+
+export default ChannelUtil;
