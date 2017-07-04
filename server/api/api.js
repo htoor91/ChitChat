3d2c79be5ab23f50970c0f8e6243b5e34c9092f1
Hassan Toor <hassantoor8@gmail.com>
2017-07-04 17:02:19 -0400

    Mount membership routes

--- server/api/api.js
+++ server/api/api.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
 router.use('/users', require('./user/user_routes'));
+router.use('/channels', require('./channel/channel_routes'));
+router.use('/memberships', require('./membership/membership_routes'));
 
 module.exports = router;
