0455fa9a2a6415ac22aeb13d586946e07ef77523
Hassan Toor <hassantoor8@gmail.com>
2017-07-04 17:02:45 -0400

    Create channel model

---
+++ server/api/channel/channel_model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ChannelSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true
+  },
+
+  private: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = mongoose.model('channel', ChannelSchema);
