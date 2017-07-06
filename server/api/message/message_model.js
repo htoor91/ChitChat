3aed9660398ab3db576323eadd5ba61cbc251ca7
Hassan Toor <hassantoor8@gmail.com>
2017-07-06 14:43:42 -0400

    Add message model

---
+++ server/api/message/message_model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MessageModel = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    index: true,
+    required: true
+  },
+  channelId: {
+    type: Schema.Types.ObjectId,
+    ref: 'channel',
+    index: true,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  chatTime: String
+}, {timestamps: true});
+
+module.exports = mongoose.model('message', MessageModel);
