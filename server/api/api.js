e89b5aae38ae85bc255212640917a5bb9dfea674
Hassan Toor <hassantoor8@gmail.com>
2017-07-06 15:52:12 -0400

    Fix typo on router

--- server/api/api.js
+++ server/api/api.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 router.use('/users', require('./user/user_routes'));
 router.use('/channels', require('./channel/channel_routes'));
 router.use('/memberships', require('./membership/membership_routes'));
-router.user('/messages', require('./message/message_routes'));
+router.use('/messages', require('./message/message_routes'));
 
 module.exports = router;
