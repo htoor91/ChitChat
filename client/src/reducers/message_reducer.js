e139979bb0d466e01d5e1d5d82bed9dd3b8218f9
Hassan Toor <hassantoor8@gmail.com>
2017-07-09 21:48:58 -0400

    Add message reducer

---
+++ client/src/reducers/message_reducer.js
@@ -0,0 +1,45 @@
+import {
+  ADD_MESSAGE,
+  REMOVE_MESSAGE,
+  EDIT_MESSAGE,
+  RECEIVE_CHANNEL_MESSAGES,
+  RECEIVE_ERRORS,
+  CLEAR_ERRORS
+} from '../actions/message_actions';
+import merge from 'lodash/merge';
+
+const initState = {
+  messages: {},
+  errors: []
+};
+
+const MessageReducer = (state = initState, action) => {
+  const nextState = merge({}, state);
+	Object.freeze(state);
+
+  switch(action.type) {
+    case ADD_MESSAGE:
+      const newMessage = action.message;
+      nextState.messages[newMessage._id] = newMessage;
+      return nextState;
+    case REMOVE_MESSAGE:
+      delete nextState.messages[action.removedMessage._id];
+      return nextState;
+    case EDIT_MESSAGE:
+      nextState.messages[action.updatedMessage._id] = action.updatedMessage;
+      return nextState;
+    case RECEIVE_CHANNEL_MESSAGES:
+      nextState.messages = action.channelMessages;
+      return nextState;
+    case RECEIVE_ERRORS:
+      nextState.errors = action.errors;
+      return nextState;
+    case CLEAR_ERRORS:
+      nextState.errors = [];
+      return nextState;
+    default:
+      return state;
+  }
+};
+
+export default MessageReducer;
