eba67be56106f4d224be7ec563f6ea15fddb52df
Hassan Toor <hassantoor8@gmail.com>
2017-07-09 21:51:42 -0400

    Add message reducer

--- client/src/reducers/root_reducer.js
+++ client/src/reducers/root_reducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import auth from './auth_reducer';
 import channels from './channel_reducer';
+import messages from './message_reducer';
 
-export default combineReducers( {auth, channels} );
+export default combineReducers( {auth, channels, messages} );
