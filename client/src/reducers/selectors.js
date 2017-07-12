c32cdf3d68f58d58b76ec0b153c003c03ca6d5f6
Hassan Toor <hassantoor8@gmail.com>
2017-07-12 18:35:59 -0400

    Fix selector issue

--- client/src/reducers/selectors.js
+++ client/src/reducers/selectors.js
@@ -1,5 +1,6 @@
 export const selectChannels = (state, bool) => {
-  const channels = Object.keys(state.channels.channels)
-                         .map(key => state.channels.channels[key]);
-  return channels.filter(channel => channel.private === bool);
+  return Object
+  .keys(state.channels.channels)
+  .map(key => state.channels.channels[key])
+  .filter(channel => channel.private === bool);
 };
