a455691570907593275a78ae37f0a10fd282723f
Hassan Toor <hassantoor8@gmail.com>
2017-07-19 20:00:32 -0400

    Add footer component

---
+++ client/src/components/home/sidebar/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Created by Hassan Toor</p>
+      <a href="#">Portfolio</a>
+      <a href="#">Github</a>
+      <a href="#">Resume</a>
+      <a href="#">Email</a>
+    </footer>
+  );
+};
+
+export default Footer;
