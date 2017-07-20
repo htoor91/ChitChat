8314a9227bbabd193b0615867529a01e0abce5b5
Hassan Toor <hassantoor8@gmail.com>
2017-07-19 20:07:20 -0400

    Add state and dispatch

--- client/src/components/home/sidebar/channel_list/channel_list_container.js
+++ client/src/components/home/sidebar/channel_list/channel_list_container.js
@@ -1,19 +1,29 @@
 import { connect } from 'react-redux';
 import ChannelList from './channel_list';
 import { selectChannels } from '../../../../reducers/selectors';
-// import { deleteChannel } from '../../../actions/channel_actions';
+import {
+  createChannel,
+  clearErrors,
+  fetchUserChannels } from '../../../../actions/channel_actions';
+import { fetchUsers } from '../../../../actions/user_actions';
 
 const mapStateToProps = (state) => {
   return({
+    user: state.auth.currentUser,
+    errors: state.channels.errors,
+    allUsers: state.users.allUsers,
     publicChannels: selectChannels(state, false),
     privateChannels: selectChannels(state, true)
   });
 };
 
-// const mapDispatchToProps = (dispatch) => {
-//   return({
-//     deleteChannel: (channelId) => dispatch(deleteChannel(channelId))
-//   });
-// };
+const mapDispatchToProps = (dispatch) => {
+  return({
+    createChannel: (channel) => dispatch(createChannel(channel)),
+    clearErrors: () => dispatch(clearErrors()),
+    fetchUsers: () => dispatch(fetchUsers()),
+    fetchUserChannels: (userId) => dispatch(fetchUserChannels(userId))
+  });
+};
 
-export default connect(mapStateToProps, null)(ChannelList);
+export default connect(mapStateToProps, mapDispatchToProps)(ChannelList);
