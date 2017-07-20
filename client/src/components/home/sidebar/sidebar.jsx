b9c820f27385cef8a22659d981b92c82d3a55230
Hassan Toor <hassantoor8@gmail.com>
2017-07-19 20:05:42 -0400

    Add logout and footer components

--- client/src/components/home/sidebar/sidebar.jsx
+++ client/src/components/home/sidebar/sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ChannelList from './channel_list/channel_list_container';
-import DirectMessages from './direct_messages/dm_list_container';
-
+import Logout from './logout';
+import Footer from './footer';
 
 class Sidebar extends React.Component {
   constructor(props){
@@ -12,13 +12,22 @@ class Sidebar extends React.Component {
     this.props.fetchUserChannels(this.props.user._id);
   }
 
+  // componentWillReceiveProps(newProps){
+  //   if (this.props.messageId === undefined &&
+  //     newProps.firstChannel !== undefined) {
+  //     newProps.history.push(`/messages/${newProps.firstChannel.id}/details`);
+  //   }
+  // }
+
   render(){
     const username = this.props.user.username;
     return(
       <div className="sidebar-container">
         <h1>ChitChat</h1>
+        <Logout logout={this.props.logout} user={this.props.user}/>
         <h2>{username}</h2>
         <ChannelList />
+        <Footer />
       </div>
     );
   }
