552f9d310e62978fca3c5a6ee9622a32363231ae
Hassan Toor <hassantoor8@gmail.com>
2017-07-23 16:31:29 -0400

    Clean up footer markup

--- client/src/components/home/sidebar/footer.jsx
+++ client/src/components/home/sidebar/footer.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="sidebar-footer">
       <p>Created by Hassan Toor</p>
-      <a href="#">Portfolio</a>
-      <a href="#">Github</a>
-      <a href="#">Resume</a>
-      <a href="#">Email</a>
+      <ul>
+        <li>
+          <a href="#">Portfolio</a>
+        </li>
+        <li>
+          <a href="#">Github</a>
+        </li>
+        <li>
+          <a href="#">Resume</a>
+        </li>
+        <li>
+          <a href="#">Email</a>
+        </li>
+      </ul>
     </footer>
   );
 };
