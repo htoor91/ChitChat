80ed540c5391089a343da8878264f6c3574dc164
Hassan Toor <hassantoor8@gmail.com>
2017-07-23 21:16:08 -0400

    Initial chat component

---
+++ client/src/components/home/chat/chat.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatHeader from './chat_header_container';
+// import ChatList from './chat/chat_list_container';
+
+const Chat = () => {
+  return (
+    <section id="chat-view-container">
+    </section>
+  );
+};
+
+export default Chat;
