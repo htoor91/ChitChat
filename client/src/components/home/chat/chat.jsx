3931192520b9120300706208018d743db2965da1
Hassan Toor <hassantoor8@gmail.com>
2017-07-25 03:20:25 -0400

    Mount ChatList component

--- client/src/components/home/chat/chat.jsx
+++ client/src/components/home/chat/chat.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ChatHeader from './chat_header_container';
-// import ChatList from './chat/chat_list_container';
+import ChatList from './chat_list/chat_list_container';
 
 const Chat = () => {
   return (
     <section id="chat-view-container">
       <ChatHeader />
+      <ChatList />
     </section>
   );
 };
