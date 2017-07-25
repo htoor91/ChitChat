1c9a2b8144bc9132711b76433e00dd9fe25229e5
Hassan Toor <hassantoor8@gmail.com>
2017-07-24 20:48:32 -0400

    Initial chat header state and props

--- client/src/components/home/chat/chat_header_container.js
+++ client/src/components/home/chat/chat_header_container.js
@@ -1,30 +1,25 @@
-// import ChatHeader from './chat_header';
-// import { connect } from 'react-redux';
-// import { withRouter } from 'react-router';
-// import { fetchChannels, fetchUserCount } from '../../../actions/channel_actions';
-// import { selectAllPublicChannels } from '../../../reducers/selectors/channel_selectors';
-//
+import ChatHeader from './chat_header';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import { fetchChannels, fetchChannelUsers } from '../../../actions/channel_actions';
 // import { deleteNotifications } from '../../../../frontend/actions/session_actions';
-//
-//
-// const mapStateToProps = (state, { match }) => {
-//   let channelId = match.params.channelId;
-//   return {
-//     user: state.session.currentUser,
-//     channel: state.channels.channels[channelId],
-//     messages: state.messages,
-//     notifications: state.session.notifications.length
-//   };
-//
-// };
-//
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     fetchChannels: (userId) => dispatch(fetchChannels(userId)),
-//     fetchUserCount: (id) => dispatch(fetchUserCount(id)),
-//     deleteNotifications: (channelId) => dispatch(deleteNotifications(channelId)),
-//   };
-//
-// };
-//
-// export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ChatHeader));
+
+
+const mapStateToProps = (state, { match }) => {
+  const channelId = match.params.channelId;
+  return {
+    user: state.auth.currentUser,
+    channel: state.channels.channels[channelId],
+    messages: state.messages.messages,
+    channelId: channelId
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchChannels: (userId) => dispatch(fetchChannels(userId)),
+    fetchChannelUsers: (channelId) => dispatch(fetchChannelUsers(channelId)),
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ChatHeader));
