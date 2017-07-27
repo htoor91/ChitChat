484419d39c3f651eda6a292ea8db56669ee16ec3
Hassan Toor <hassantoor8@gmail.com>
2017-07-26 20:10:45 -0400

    Add socket events for chat

--- server/socket_events.js
+++ server/socket_events.js
@@ -1,13 +1,24 @@
 module.exports = function(io){
   io.on('connection', function(socket){
+    console.log('USER HAS CONNECTED');
 
-    socket.on('new message', function(message){
-      socket.broadcast.emit('new message', message);
+    socket.on('disconnect', function(){
+      console.log('USER HAS DISCONNECTED');
     });
 
+    socket.on('join channel', function(data){
+      console.log("JOINING CHANNEL: " + data.channel);
+      socket.join(data.channel);
+    });
 
+    socket.on('leave channel', function(data){
+      console.log("LEAVING CHANNEL: " + data.channel);
+      socket.leave(data.channel);
+    });
 
-
-
+    socket.on('broadcast message', function(data){
+      console.log("BROADCASTING MESSAGE FROM: " + data.channel);
+      socket.broadcast.to(data.channel).emit('receive message', data);
+    });
   });
 };
