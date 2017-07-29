d21298f6d8801cf78146b49c89092ac38db25129
Hassan Toor <hassantoor8@gmail.com>
2017-07-29 18:29:59 -0400

    Add notifications

--- client/src/components/home/sidebar/channel_list/list_item.jsx
+++ client/src/components/home/sidebar/channel_list/list_item.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 const ListItem = ({ channel }) => {
   const name = channel.private ? `@${channel.name}` : `#${channel.name}`;
   const link = channel._id;
+  const notification = channel.notifications ? channel.notifications : "";
 
   return (
     <li className="list-item">
       <a href={`#/messages/${link}`} className="list-item-link">
-        <h5 className="list-item-name">{name}</h5>
+        <h5 className="list-item-name">{name}
+          <span id="notifications">{notification}</span>
+        </h5>
       </a>
     </li>
   );
