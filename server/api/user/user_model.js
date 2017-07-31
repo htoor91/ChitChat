b4d8538557615fc725a04ae154fb0fed5599eb2d
Hassan Toor <hassantoor8@gmail.com>
2017-07-31 18:42:35 -0400

    Add aviUrl to model on prehook

--- server/api/user/user_model.js
+++ server/api/user/user_model.js
@@ -14,6 +14,10 @@ const UserSchema = new Schema({
   password: {
     type: String,
     required: true
+  },
+
+  aviUrl: {
+    type: String
   }
 });
 
@@ -22,6 +26,22 @@ UserSchema.pre('save', function(next) {
 
 
   this.password = this.encryptPassword(this.password);
+  next();
+});
+
+UserSchema.pre('save', function(next){
+  const aviUrls = [
+    'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_1_bksazr.png',
+    'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_2_jwwm4s.png',
+    'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_3_ov1pzy.png',
+    'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_4_n9oahl.png',
+    'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_5_veiq3b.png',
+    'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_6_nh7wgh.png'
+  ];
+  const randomIndex = Math.floor(Math.random() * aviUrls.length);
+  const aviUrl = aviUrls[randomIndex];
+  this.aviUrl = aviUrl;
+
   next();
 });
 
