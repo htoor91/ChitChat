70af4a814f84dfad12418057c8d52cc145b57e86
Hassan Toor <hassantoor8@gmail.com>
2017-07-31 18:43:25 -0400

    Add detail view component

---
+++ client/src/components/home/chat/chat_list/detail.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+
+class DetailView extends React.Component{
+    constructor(props){
+      super(props);
+      this.closeDetailView = this.closeDetailView.bind(this);
+    }
+
+    closeDetailView(){
+      this.props.history.push(`/messages/${this.props.channelId}`);
+    }
+
+    render(){
+      if (this.props.channel === undefined) return <p></p>;
+      const name = this.props.channel.private ? "this DM" :
+      "#" + this.props.channel.name;
+      let userCount;
+      let users;
+      let userList;
+
+      if(this.props.channel.users){
+        userCount = this.props.channel.users.length;
+        users = this.props.channel.users;
+        userList = users.map((user) => (
+          <li key={user._id} id="details-user-list-item">
+            <img src={user.aviUrl} />
+            <span id="detail-view-username">{user.username}</span>
+          </li>
+        ));
+      }
+
+      return (
+        <section id="detail-view">
+          <div id="detail-view-header">
+            <h2 id="about-team-header">Members of {name}</h2>
+            <i onClick={this.closeDetailView} id="detail-view-exit" className="fa fa-times" aria-hidden="true"></i>
+          </div>
+          <div id="detail-user-info">
+            <div id="detail-user-count-header">
+              <h3> <i id="detail-fa" className="fa fa-user-o" aria-hidden="true"></i>
+                {userCount} members
+              </h3>
+            </div>
+            <ul id="detail-user-list">
+              {userList}
+            </ul>
+          </div>
+        </section>
+      );
+    }
+}
+export default withRouter(DetailView);
