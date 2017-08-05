d0f5c33770a2b21690bc9c14fabe232bd3b0e7a1
Hassan Toor <hassantoor8@gmail.com>
2017-08-04 20:40:30 -0400

    Add 3rd party giphy API util

---
+++ client/src/util/giphy_util.js
@@ -0,0 +1,19 @@
+const GiphyUtil = {
+
+  fetchGifs(searchTerm){
+    return $.ajax({
+      method: 'GET',
+      url: `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=6b7cfeaff97c49aa8af86bd5f8ea9b83&limit=15`
+    });
+  },
+
+  translateToGif(searchTerm){
+    return $.ajax({
+      method: 'GET',
+      url: `https://api.giphy.com/v1/gifs/translate?s=${searchTerm}&api_key=6b7cfeaff97c49aa8af86bd5f8ea9b83`
+    });
+  }
+};
+
+
+export default GiphyUtil;
