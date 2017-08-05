2592e1a96d5e083434238e7840e6f173d0da5a1b
Hassan Toor <hassantoor8@gmail.com>
2017-08-04 20:34:47 -0400

    Add giphy reducer

---
+++ client/src/reducers/giphy_reducer.js
@@ -0,0 +1,25 @@
+import { RECEIVE_GIFS } from '../actions/giphy_actions';
+import merge from 'lodash/merge';
+
+
+const initState = {
+  giphys: []
+};
+
+const GiphysReducer = (state = initState, action) => {
+  const nextState = merge({}, state);
+  Object.freeze(state);
+
+  switch(action.type) {
+    case RECEIVE_GIFS:
+      nextState.giphys = action.giphys;
+      return nextState;
+    // case RECEIVE_GIF:
+    //   nextState.gif = action.giphy;
+    //   return nextState;
+    default:
+      return state;
+  }
+};
+
+export default GiphysReducer;
