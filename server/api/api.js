4a33db73cfdab494b3c9e75bc967bdb3bf07b350
Hassan Toor <hassantoor8@gmail.com>
2017-08-04 20:42:25 -0400

    Mount emoticons routes

--- server/api/api.js
+++ server/api/api.js
@@ -4,5 +4,6 @@ router.use('/users', require('./user/user_routes'));
 router.use('/channels', require('./channel/channel_routes'));
 router.use('/memberships', require('./membership/membership_routes'));
 router.use('/messages', require('./message/message_routes'));
+router.use('/emoticons', require('./emoticon/emoticon_routes'));
 
 module.exports = router;
