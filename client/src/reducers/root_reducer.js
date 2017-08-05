3cba7e36dc21a1046d531cd32ea7db01951aeb38
Hassan Toor <hassantoor8@gmail.com>
2017-08-04 20:35:25 -0400

    Add giphy reducer to root reducer

--- client/src/reducers/root_reducer.js
+++ client/src/reducers/root_reducer.js
@@ -3,5 +3,6 @@ import auth from './auth_reducer';
 import channels from './channel_reducer';
 import messages from './message_reducer';
 import users from './user_reducer';
+import giphys from './giphy_reducer';
 
-export default combineReducers( {auth, channels, messages, users} );
+export default combineReducers( {auth, channels, messages, users, giphys} );
