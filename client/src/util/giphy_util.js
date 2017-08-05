aadadb2f78627276e8a6f3a1b9c43e8d5f028613
Hassan Toor <hassantoor8@gmail.com>
2017-08-05 16:43:40 -0400

    Change image limit

--- client/src/util/giphy_util.js
+++ client/src/util/giphy_util.js
@@ -3,7 +3,7 @@ const GiphyUtil = {
   fetchGifs(searchTerm){
     return $.ajax({
       method: 'GET',
-      url: `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=6b7cfeaff97c49aa8af86bd5f8ea9b83&limit=15`
+      url: `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=6b7cfeaff97c49aa8af86bd5f8ea9b83&limit=18`
     });
   },
 
