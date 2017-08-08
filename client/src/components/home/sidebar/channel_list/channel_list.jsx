a40a9cd54f92ece99a875284a7cad4805ea73f21
Hassan Toor <hassantoor8@gmail.com>
2017-08-08 00:09:12 -0400

    Change modal style

--- client/src/components/home/sidebar/channel_list/channel_list.jsx
+++ client/src/components/home/sidebar/channel_list/channel_list.jsx
@@ -21,10 +21,8 @@ class ChannelList extends React.Component {
         left                  : '50%',
         right                 : 'auto',
         bottom                : 'auto',
-        marginRight           : '-50%',
         transform             : 'translate(-50%, -50%)',
         width                 : '100%',
-        height                : '140%'
       },
       overlay: {
         zIndex                : '1000'
