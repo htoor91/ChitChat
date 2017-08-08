ec13b6de8d3227bb15330ac939e036ea192bc7f7
Hassan Toor <hassantoor8@gmail.com>
2017-08-08 12:46:15 -0400

    Fix up links and icons

--- client/src/components/home/sidebar/footer.jsx
+++ client/src/components/home/sidebar/footer.jsx
@@ -6,16 +6,39 @@ const Footer = () => {
       <p>Created by Hassan Toor</p>
       <ul>
         <li>
-          <a href="#">Portfolio</a>
+          <a href="http://hassantoor.com" target="_blank">
+            <i className="fa fa-code"
+              aria-hidden="true"></i>
+            Portfolio
+          </a>
         </li>
         <li>
-          <a href="#">Github</a>
+          <a href="http://github.com/htoor91" target="_blank">
+            <i className="fa fa-github"
+              aria-hidden="true"></i>
+            Github
+          </a>
         </li>
         <li>
-          <a href="#">Resume</a>
+          <a href="http://linkedin.com/in/hassantoor" target="_blank">
+            <i className="fa fa-linkedin"
+              aria-hidden="true"></i>
+            LinkedIn
+          </a>
         </li>
         <li>
-          <a href="#">Email</a>
+          <a href="http://hassantoor.com/assets/Resume.pdf" target="_blank">
+            <i className="fa fa-file-text-o"
+              aria-hidden="true"></i>
+            Resume
+          </a>
+        </li>
+        <li>
+          <a href="mailto:hassantoor8@gmail.com" target="_blank">
+            <i className="fa fa-envelope-o"
+              aria-hidden="true"></i>
+            Email
+          </a>
         </li>
       </ul>
     </footer>
