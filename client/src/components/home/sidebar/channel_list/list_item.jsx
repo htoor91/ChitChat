79a58f2235d0b333c931fa16d483dcf130438787
Hassan Toor <hassantoor8@gmail.com>
2017-08-08 12:47:16 -0400

    Add white channel name on notification

--- client/src/components/home/sidebar/channel_list/list_item.jsx
+++ client/src/components/home/sidebar/channel_list/list_item.jsx
@@ -3,14 +3,26 @@ import React from 'react';
 const ListItem = ({ channel }) => {
   const name = channel.private ? `@${channel.name}` : `#${channel.name}`;
   const link = channel._id;
-  const notification = channel.notifications ? channel.notifications : "";
+  let listItemName;
+
+
+  if(channel.notifications){
+    const notification = channel.notifications;
+    listItemName = (
+      <h5 className="list-item-name white-notification">{name}
+        <span id="notifications">{notification}</span>
+      </h5>
+    );
+  } else {
+    listItemName = (
+      <h5 className="list-item-name">{name}</h5>
+    );
+  }
 
   return (
     <li className="list-item">
       <a href={`#/messages/${link}`} className="list-item-link">
-        <h5 className="list-item-name">{name}
-          <span id="notifications">{notification}</span>
-        </h5>
+        {listItemName}
       </a>
     </li>
   );
