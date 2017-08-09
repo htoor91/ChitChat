6138b68d42d2b5f7ae09dbbe52531e71b7063c4c
Hassan Toor <hassantoor8@gmail.com>
2017-08-09 00:32:24 -0400

    Add appropriate css class names

--- client/src/components/splash/splash.jsx
+++ client/src/components/splash/splash.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import AuthFormContainer from './auth_form/auth_form_container';
-import logo from '../../public/img/splash-logo.png';
+import logo from '../../public/img/chat.png';
 import { AuthRoute } from '../../util/route_util';
 
 const Splash = ({children}) => {
 
   return(
-    <section id="splash-main">
-      <img id="logo" src={logo}/>
-      <h1>ChitChat</h1>
-      <Link to="/login">Login</Link>
-      <Link to="/signup">Signup</Link>
+    <section className="splash-container">
+      <div className="splash-wrapper">
+        <img src={logo}/>
+        <h1 className="welcome-page-header">ChitChat</h1>
+        <h2 className="welcome-page-subheading">Where work happens.</h2>
+        <div id="welcome-page-buttons">
+          <Link className="splash-button" to="/login">Login</Link>
+          <Link className="splash-button" to="/signup">Signup</Link>
+        </div>
+      </div>
     </section>
   );
 };
