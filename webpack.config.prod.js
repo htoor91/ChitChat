fc01ce05bd6c944362da5f5e43b0917a58359cd8
Hassan Toor <hassantoor8@gmail.com>
2017-08-10 23:00:42 -0400

    Fix path for bundle

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,7 +6,7 @@ module.exports = {
   context: __dirname,
   entry: './client/src/chitchat.jsx',
   output: {
-    path: path.join(__dirname, 'js'),
+    path: "./client/src/public/js",
     filename: "bundle.js"
   },
   plugins:[
