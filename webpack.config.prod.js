71eee5dfc6b70431f3332151e0ef8343cf0a8573
Hassan Toor <hassantoor8@gmail.com>
2017-08-10 22:37:40 -0400

    Add production webpack file

---
+++ webpack.config.prod.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const webpack = require("webpack");
+
+
+module.exports = {
+  context: __dirname,
+  entry: './client/src/chitchat.jsx',
+  output: {
+    path: path.join(__dirname, 'client', 'src', 'public'),
+    filename: "bundle.js"
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: "url-loader?limit=5000&name=img/img-[hash:6].[ext]"
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
