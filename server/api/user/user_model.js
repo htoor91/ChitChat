23a2177d949dd51de5f4411584cafc8c4a3c5a40
Hassan Toor <hassantoor8@gmail.com>
2017-08-11 18:05:55 -0400

    Fix avi bug

--- server/api/user/user_model.js
+++ server/api/user/user_model.js
@@ -29,17 +29,19 @@ UserSchema.pre('save', function(next) {
 });
 
 UserSchema.pre('save', function(next){
-  const aviUrls = [
-    'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_1_bksazr.png',
-    'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_2_jwwm4s.png',
-    'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_3_ov1pzy.png',
-    'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_4_n9oahl.png',
-    'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_5_veiq3b.png',
-    'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_6_nh7wgh.png'
-  ];
-  const randomIndex = Math.floor(Math.random() * aviUrls.length);
-  const aviUrl = aviUrls[randomIndex];
-  this.aviUrl = aviUrl;
+  if(!this.aviUrl){
+    const aviUrls = [
+      'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_1_bksazr.png',
+      'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_2_jwwm4s.png',
+      'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_3_ov1pzy.png',
+      'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_4_n9oahl.png',
+      'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_5_veiq3b.png',
+      'https://res.cloudinary.com/htoor91/image/upload/v1501461853/chitchat_6_nh7wgh.png'
+    ];
+    const randomIndex = Math.floor(Math.random() * aviUrls.length);
+    const aviUrl = aviUrls[randomIndex];
+    this.aviUrl = aviUrl;
+  }
 
   next();
 });
