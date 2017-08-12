e5e83941993d09f2016fa244ba76e3ef8ded73df
Hassan Toor <hassantoor8@gmail.com>
2017-08-11 21:31:53 -0400

    Reseed

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ if (config.seed) {
   require('./util/seed');
 }
 
+
 app.use(express.static(path.join(__dirname, '..', 'client', 'src', 'public')));
 
 require('./middleware/app_middleware')(app);
